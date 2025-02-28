939eaeb87a9301e16b903055bd906fc7603c9280
leeminjin0827 <lmj98202@gmail.com>
2025-02-28 13:32:05 +0900

    ..

---
+++ Ex03_JavaCodeDI/src/main/java/com/example/demo/Ex03JavaCodeDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex03JavaCodeDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex03JavaCodeDiApplication.class, args);
+	}
+
+}
